524d844b51a078147cab996ee1c204610252cc37
rohanC-dev <rohan.chopra241@gmail.com>
2021-06-07 16:45:47 -0400

    save point

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { json } = require("body-parser");
+
 var express = require("express"),
         app = express(),
   mongoose  = require("mongoose"),
@@ -46,8 +48,9 @@ app.get("/comic/:num", function(req, res){
 		if(!error && response.statusCode == 200){
 
             //res.render("home.ejs", {data: JSON.parse(body)});
-
-            parseTranscript(body);
+            //splitTranscript(body);
+            //parseTranscript(body);
+            
             Comic.find({id: currentComic}, function(err, retrievedComic){
                 if(err){
                     console.log("there was an error");
@@ -65,7 +68,8 @@ app.get("/comic/:num", function(req, res){
                                         console.log("there was an error");
                                         console.log(err); 
                                     }
-                                    res.render("home.ejs", {data: JSON.parse(body), views: updatedComic.views});   
+                                    res.render("home.ejs", {data: JSON.parse(body), views: updatedComic.views});
+                                    splitTranscript(body);   
                                 });
                             }
                         });
@@ -75,7 +79,8 @@ app.get("/comic/:num", function(req, res){
                                 console.log("there was an error");
                                 console.log(err); 
                             }
-                            res.render("home.ejs", {data: JSON.parse(body), views: updatedComic.views});   
+                            res.render("home.ejs", {data: JSON.parse(body), views: updatedComic.views});
+                            splitTranscript(body);   
                         });
                     }
 
@@ -86,10 +91,36 @@ app.get("/comic/:num", function(req, res){
 	});
 });
 
-function parseTranscript(body){
-    console.log(JSON.parse(body).transcript);
+function splitTranscript(body){
     var transcriptString = JSON.parse(body).transcript;
+    var linesOfTranscript = [];
+    for(var i = 0; i < transcriptString.length; i++){
+        if(transcriptString.substring(i, i+1) === "\n"){
+            var j = i+2;
+            while(transcriptString.charAt(j) != "\\" && transcriptString.charAt(j+1) != "n"){
+                j++;
+            }
 
+            linesOfTranscript.push({
+                string: transcriptString.substring(i+2,j)
+            });
+        }
+    }
+
+    //console.log(linesOfTranscript);
+
+    var strings = transcriptString.split("\n");
+    console.log(strings);
+
+
+}
+
+
+
+function parseTranscript(body){
+    //console.log(JSON.parse(body).transcript);
+    var transcriptString = JSON.parse(body).transcript;
+    console.log(transcriptString);
     var formattedTranscript = [];
 
     for(var i = 0; i < transcriptString.length; i++){
@@ -118,10 +149,27 @@ function parseTranscript(body){
                 string: transcriptString.substring(i+2,j),
                 type: "<"
             });
+        }else if(transcriptString.charAt(i) === ":"){
+            var j;
+            
+            for(j = i; (transcriptString.substring(j-1, j) != "\n"); j--){
+                
+                //console.log(transcriptString.charAt(j));
+            }
+
+            for(k = j; transcriptString.charAt(k) != "\\"; k++){
+                //console.log(transcriptString.charAt(j));
+            }
+
+            formattedTranscript.push({
+                string: transcriptString.substring(j,k),
+                type: "dialogue"
+            });
+
         }
     }
     console.log(formattedTranscript);
-    
+    return transcriptString;
 }
 
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
